c037d1afc3d6630ebbf68901de777c563a349c6f
Markuss Tumans <94705240+Sculzi@users.noreply.github.com>
2022-10-19 04:18:22 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,28 @@
-human_turn = 'Paper'
-computer_turn = 'Rock'
+contacts = []
 
-if human_turn == computer_turn:
-    print('Draw!')
-elif human_turn == 'Rock' and computer_turn == 'Scissors':
-    print('Human wins!')
-elif human_turn == 'Paper' and computer_turn == 'Rock':
-    print('Human wins!')
-elif human_turn == 'Scissors' and computer_turn == 'Paper':
-    print('Human wins!')
-else:
-    print('Computer wins!')
+while(True):
+    response = input('(1)Add Contact (2)Print Contacts (3)Exit: ')
+    
+    if response == '1':
+        person_name = input('Name: ')
+        person_surname = input('Surname: ')
+        person_phone = input('Phone: ')
+        person_email = input('Email: ')
+
+        person_data = {
+            'Name': person_name,
+            'Surname': person_surname,
+            'Phone': person_phone,
+            'Email': person_email
+        }
+
+        contacts.append(person_data)
+    elif response == '2':
+        for contact in contacts:
+            print("---CONTACT---")
+            print(f"{contact['Name']} {contact['Surname']}")
+            print(f"Phone: {contact['Phone']}")
+            print(f"Email: {contact['Email']}")
+    elif response == '3':
+        print('Bye!')
+        exit()
